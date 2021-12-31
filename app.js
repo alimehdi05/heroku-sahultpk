a6d275390c51471c7b041cb1787115931e036c12
alimehdi05 <91363582+alimehdi05@users.noreply.github.com>
2021-12-31 16:06:40 +0500

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var mongoose = require("mongoose");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/api/users");
 var productsRouter = require("./routes/api/products");
-var getsRouter = require("./routes/api/gets");
-var contactsRouter = require("./routes/api/contacts");
 var config = require("config");
 var cors = require("cors");
 var app = express();
@@ -26,8 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/products", productsRouter);
-app.use("/api/gets", getsRouter);
-app.use("/api/contacts", contactsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
